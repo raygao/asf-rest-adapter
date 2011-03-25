59417a88726b8b87c9d7a1ba536705a7226f97b6
Raymond Gao <raygao2000@yahoo.com>
2011-03-25 13:04:01 -0500

    fix typo 2

--- lib/Salesforce/rest/authenticate.rb
+++ lib/Salesforce/rest/authenticate.rb
@@ -15,63 +15,66 @@
 
 module Salesforce
   module Rest
-    module Authentication
-      # Initializes the adapter, using username, password. A good place to invoke
-      # it includes 'setup()' method in the 'test_helper' and Rails init files.
-      # usage ->     bootup_rest_adapter(username, password, api_version)
-      def self.bootup_rest_adapter(username, password, api_version)
-        p "*" * 80
-        p 'Set up code'
 
-        login_svr = 'https://login.salesforce.com'
-        api_version = api_version ? api_version : '21.0'
+    class AsfRest < ActiveResource::Base
+      module Authentication
+        # Initializes the adapter, using username, password. A good place to invoke
+        # it includes 'setup()' method in the 'test_helper' and Rails init files.
+        # usage ->     bootup_rest_adapter(username, password, api_version)
+        def self.bootup_rest_adapter(username, password, api_version)
+          p "*" * 80
+          p 'Set up code'
 
-        uri = URI.parse(login_svr)
-        uri.path = "/services/Soap/u/" + (api_version).to_s
-        url = uri.to_s
+          login_svr = 'https://login.salesforce.com'
+          api_version = api_version ? api_version : '21.0'
 
-        binding = RForce::Binding.new(url, nil, nil)
-        soap_response = binding.login(username, password)
-        soap_server_url = soap_response.loginResponse.result.serverUrl
-        security_token = soap_response.loginResponse.result.sessionId
-        user_id = soap_response.loginResponse.result.userId
-        puts "binding user id is: " + user_id
+          uri = URI.parse(login_svr)
+          uri.path = "/services/Soap/u/" + (api_version).to_s
+          url = uri.to_s
 
-        rest_svr = soap_server_url.gsub(/-api\S*/mi, "") + ".salesforce.com"
-        rest_version = "v" + api_version
+          binding = RForce::Binding.new(url, nil, nil)
+          soap_response = binding.login(username, password)
+          soap_server_url = soap_response.loginResponse.result.serverUrl
+          security_token = soap_response.loginResponse.result.sessionId
+          user_id = soap_response.loginResponse.result.userId
+          puts "binding user id is: " + user_id
 
-        self.setup(security_token, rest_svr, rest_version)
-        puts "oauth token is: " + security_token
+          rest_svr = soap_server_url.gsub(/-api\S*/mi, "") + ".salesforce.com"
+          rest_version = "v" + api_version
 
-        puts 'rest_svr' + rest_svr
+          self.setup(security_token, rest_svr, rest_version)
+          puts "oauth token is: " + security_token
 
-        return [security_token, rest_svr, rest_version]
+          puts 'rest_svr' + rest_svr
+
+          return [security_token, rest_svr, rest_version]
+        end
+
+
+        # Initializes the adapter, the 1st step of using the adapter. A good place to invoke
+        # it includes 'setup()' method in the 'test_helper' and Rails init file.
+        # TODO, to be removed in the 1.0 version
+        # usage ->     bootup_rest_adapter_old_adapter()
+        def self.bootup_rest_adapter_with_old_adapter()
+          require 'asf-soap-adapter'
+          p "*" * 80
+          p 'Set up code'
+          @u = Salesforce::User.first
+          @version = "v" + @u.connection.config[:api_version].to_s
+          puts "Sf User name is: " + @u.name
+
+          @oauth_token = @u.connection.binding.instance_variable_get("@session_id")
+          puts "oauth token is: " + @oauth_token
+
+          @soap_url = @u.connection.binding.instance_variable_get("@server").address
+          @rest_svr = @soap_url.gsub(/-api\S*/mi, "") + ".salesforce.com"
+          puts 'rest_svr' + @rest_svr
+
+          self.setup(@oauth_token, @rest_svr, @version)
+          return [@oauth_token, @rest_svr, @version]
+        end
+
       end
-
-
-      # Initializes the adapter, the 1st step of using the adapter. A good place to invoke
-      # it includes 'setup()' method in the 'test_helper' and Rails init file.
-      # TODO, to be removed in the 1.0 version
-      # usage ->     bootup_rest_adapter_old_adapter()
-      def self.bootup_rest_adapter_with_old_adapter()
-        require 'asf-soap-adapter'
-        p "*" * 80
-        p 'Set up code'
-        @u = Salesforce::User.first
-        @version = "v" + @u.connection.config[:api_version].to_s
-        puts "Sf User name is: " + @u.name
-
-        @oauth_token = @u.connection.binding.instance_variable_get("@session_id")
-        puts "oauth token is: " + @oauth_token
-
-        @soap_url = @u.connection.binding.instance_variable_get("@server").address
-        @rest_svr = @soap_url.gsub(/-api\S*/mi, "") + ".salesforce.com"
-        puts 'rest_svr' + @rest_svr
-
-        self.setup(@oauth_token, @rest_svr, @version)
-        return [@oauth_token, @rest_svr, @version]
-      end
-
     end
   end
 end
