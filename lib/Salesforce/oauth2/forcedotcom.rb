25210a00e89af76f83f9ecf747f07aeda603f883
Raymond Gao <raygao2000@yahoo.com>
2011-05-23 18:02:11 -0500

    changing to use 'token' instead of 'code' to work across all sites, bascially rendering the consumer key/secret combo as a placeholder. Now, Chatter.com does work

--- lib/Salesforce/oauth2/forcedotcom.rb
+++ lib/Salesforce/oauth2/forcedotcom.rb
@@ -1,41 +1,49 @@
-require 'omniauth/oauth'
-require 'multi_json'
+#require 'omniauth/oauth'
+#require 'multi_json'
 
-# Omniauth strategy for using oauth2 and force.com 
-# Author: qwall@salesforce.com
-#
+require 'omniauth/strategies/oauth2'
 module OmniAuth
   module Strategies
-    class Forcedotcom < OAuth2
-      # Initialize the middleware
-      #
-      # @option options [Boolean, true] :sign_in When true, use a sign-in flow instead of the authorization flow.
-      # @option options [Boolean, false] :mobile When true, use the mobile sign-in interface.
-      def initialize(app, client_id = nil, client_secret = nil, options = {}, &block)
+    class Forcedotcom <  OmniAuth::Strategies::OAuth2
+
+      def initialize(app, consumer_key = nil, consumer_secret = nil, options = {}, &block)
         client_options = {
-          :site => "https://login.salesforce.com",
-          :authorize_path      => "/services/oauth2/authorize",
-          :access_token_path   => "/services/oauth2/token"
+          :site => 'https://login.salesforce.com',
+          :authorize_path => '/services/oauth2/authorize',
+          :access_token_path => '/services/oauth2/token'
         }
-        super(app, :forcedotcom, client_id, client_secret, client_options, &block) 
+
+        # 'code' locks you into one org; 'token' works across all orgs.
+        options.merge!(:response_type => 'token', :grant_type => 'authorization_code')
+
+        super(app, :forcedotcom, consumer_key, consumer_secret, client_options, options, &block)
       end
 
-      def request_phase
-        options[:response_type] ||= 'code'
-        super
-      end
-
-      def callback_phase
-        options[:grant_type] ||= 'authorization_code'
-        super
-      end
-
-       def auth_hash
-          OmniAuth::Utils.deep_merge(super, {
-            'instance_url' => @access_token['instance_url']
+      def auth_hash
+        data = user_data
+        OmniAuth::Utils.deep_merge(super, {
+            'uid' => @access_token['id'],
+            'credentials' => {
+              'instance_url' => @access_token['instance_url']
+            },
+            'extra' => {'user_hash' => data},
+            'user_info' => {
+              'email' => data['email'],
+              'name' => data['display_name']
+            }
           })
-       end
-      
+      end
+
+      def user_data
+        @data ||= MultiJson.decode(@access_token.get(@access_token['id']))
+      rescue ::OAuth2::HTTPError => e
+        if e.response.status == 302
+          @data ||= MultiJson.decode(@access_token.get(e.response.headers['location']))
+        else
+          raise e
+        end
+      end
     end
+
   end
 end
